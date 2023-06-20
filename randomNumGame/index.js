a0cfc4f02faca882489adfc568d5ed0ed5a146b9
Seojoon Hwang <seojoonhwang@Seojoonui-MacBookPro.local>
2023-06-20 15:35:59 +0900

    random number game

---
+++ randomNumGame/index.js
@@ -0,0 +1,37 @@
+const form = document.querySelector(".numForm");
+const range = document.querySelector(".rangeInput");
+const inputNum = form.querySelector(".inputNum");
+const maxNum = document.querySelector(".maxNum");
+const paintChose = document.querySelector(".paintChose");
+const paintResult = document.querySelector(".paintResult");
+
+function result(myNum, randNum) {
+  paintChose.innerText = `You chose: ${myNum}, the machine chose ${randNum}`;
+  myNum = parseInt(myNum);
+  if (myNum === randNum) {
+    paintResult.innerText = `You won!`;
+  } else {
+    paintResult.innerText = `You lose!`;
+  }
+}
+
+function makeRandNum(e) {
+  e.preventDefault();
+  const myNum = inputNum.value;
+  const maxValue = range.value;
+  const randNum = Math.ceil(Math.random() * maxValue);
+
+  result(myNum, randNum);
+}
+
+function handleInput() {
+  const maxNumValue = range.value;
+
+  maxNum.innerText = `${maxNumValue}`;
+  form.addEventListener("submit", makeRandNum);
+}
+
+function init() {
+  range.oninput = handleInput;
+}
+init();
